5eacb2d6360e761827a2c7ce455fcebafc3b3e13
Jakub Wójcik <athwojcik@gmail.com>
2022-12-01 23:41:42 +0100

    14.0 - SpringData&Jpa - introduce h2 database and spring data&jpa configurations

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,8 +1,4 @@
 
-?
-settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
-X
-(gradle/wrapper/gradle-wrapper.properties,f\b\fbe448ebfc3eb2d4e308f6b8b043666f5b57235e
 7
 gradlew,5\b\5bbfa66edb4db3c7c33c5181f43510990d3307f9
 ;
--- data/src/main/java/unisystem/domain/User.java
+++ data/src/main/java/unisystem/domain/User.java
@@ -3,9 +3,15 @@ package unisystem.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.*;
+
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name="users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String login;
     private String password;
